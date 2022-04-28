f6eef4c85839f5e1f8655b7d3b29dc6fd669e233
Brimey <46639136+Brimey@users.noreply.github.com>
2022-04-28 11:21:18 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,11 @@ import json
 
 
 def rename_file(cwd=os.getcwd()):
+    # Make sure to set the file location of the downloaded chat to the same directory as this program.
     """
     Renames the arbitrary .json file name to a generic format
-    after the vod chat has been downloaded via TwitchDownloader.
+    that's easier to work with after the vod chat has been
+    downloaded via TwitchDownloader.
 
     Keyword Arguments:
         cwd - A string representing the current working directory.
@@ -17,14 +19,18 @@ def rename_file(cwd=os.getcwd()):
 
 def parse_json():
     """
-    Reads JSON file and cleans up the data.
+    Reads the JSON file and cleans up the data.
     """
     with open('downloaded_chat.json', 'r', encoding="utf-8") as file:
         contents = json.load(file)
+        chat_history, user_comments = contents['comments'], {}
+        for data in chat_history:
+            print(data, end='\n\n')  # Separating with a new lines to get a better visual on data. Temporary code.
 
 
 def main():
     rename_file()
+    parse_json()
 
 
 main()
